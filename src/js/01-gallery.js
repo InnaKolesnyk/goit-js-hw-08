5d47d4d3239aceb5744b2d9e6c03c7bfe3dce339
InnaKolesnyk <inna.ivanovna.k@gmail.com>
2022-11-08 14:03:05 +0200

    hw-js-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+//console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const createImg = galleryItems
+  .map(
+    element =>
+      `<li class = "gallery__item">
+      <a href ="${element.original}" class ="gallery__link">
+      <img class="gallery__image" src="${element.preview}" alt="${element.description}" data-source = "${element.original}">
+      </a>
+      </li>`
+  )
+  .join('');
+  
+    gallery.insertAdjacentHTML('beforeend', createImg);
+
+let galleryImg = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player'
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let i = Number(localStorage.getItem('videoplayer-current-time')) || 0;
+player.on(
+  'timeupdate',
+  throttle(function (event) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(event.seconds)
+    );
+  }, 1000)
+);
+
+player
+  .setCurrentTime(i)
+  .then(function (seconds) {
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
